1adee90fff04d1d5cbf45f9a0ab82f797db8b732
Lucas Hoskem <lucashoskem2@gmail.com>
2024-07-01 08:57:29 -0300

    first commit

--- src/components/botao/botao.jsx
+++ src/components/botao/botao.jsx
@@ -1,6 +1,13 @@
-
+import styles from './botao.module.css'
 
 
 function botao(){
-    
+    return (
+        <div class="box-3">
+            <div class="btn btn-three">
+                <span>TESTE</span>
+            </div>
+        </div>
+    )
 }
+export default botao 
---
+++ src/components/lista/lista.jsx
@@ -0,0 +1,11 @@
+import styles from './lista.module.css'
+
+
+function Lista(){
+    return (
+        <ul>
+            <li></li>
+        </ul>
+    )
+}
+export default Lista
